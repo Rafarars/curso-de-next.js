c51659a2e6bd9f0e8e0b2a34a48d0258cfc0d2db
Rafael Rodriguez <rafa.sosa20@gmail.com>
2025-01-31 16:46:45 -0400

    capitulo 3 finalizado

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,9 @@
 import { Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 export const lusitana = Lusitana({
     weight: ['400', '700'], // Ajusta los pesos según tus necesidades
     subsets: ['latin'],
 });
+
+export const inter = Inter({ subsets: ['latin'] });
